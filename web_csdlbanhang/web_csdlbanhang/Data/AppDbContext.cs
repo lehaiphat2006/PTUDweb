9fd99e358203cafdbd7ed56afb3c582f43fda5a2
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-09-17 20:59:31 +0700

    update

---
+++ web_csdlbanhang/web_csdlbanhang/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_csdlbanhang.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) { }
+    }
+}
